48872b802faff88c1c363457a14eb4014f975352
xavier <xavier.buhot12358@gmail.com>
2018-05-15 21:34:47 +0200

    WIP 2

--- include/pam_face_module/face_detector.h
+++ include/pam_face_module/face_detector.h
@@ -1,11 +1,48 @@
 #ifndef FACE_DETECTOR_H
 #define FACE_DETECTOR_H
 
+#include <memory>
+#include <tensorflow/c/c_api.h>
 
-class Face_detector
-{
+class Face_detector {
+
+    // Load graph.
+    std::shared_ptr<TF_Graph> graph_;
+    std::shared_ptr<TF_Status> status_;
+
+    // Create session.
+    std::shared_ptr<TF_Session> sess_;
+
 public:
-    Face_detector();
+  Face_detector();
+
+private:
+
+  /////////////
+  /// \brief createSession
+  /// \return
+  ///
+  bool createSession();
+
+  ////////////////
+  /// \brief Face_detector::init
+  /// \param fileName
+  ///
+  void init(std::string const fileName);
+
+
+  ////////////////////
+  /// \brief Face_detector::loadGraph
+  /// \param fileName
+  ///
+  bool loadGraph(std::string const fileName);
+
+  /////////////////
+  /// \brief Face_detector::read_file
+  /// \param fileName
+  /// \return
+  ///
+  std::shared_ptr<TF_Buffer> read_file(std::string const fileName);
 };
 
 #endif // FACE_DETECTOR_H
---
+++ src/pam_face_module/pnet.cpp
@@ -0,0 +1,3 @@
+#include "pam_face_module/pnet.h"
+
+Pnet::Pnet(std::shared_ptr<TF_Graph> graph) { graph_ = graph; }
